9acee07860bd97bd61dded76d23874ec268778dc
mateo <mateofeit@gmail.com>
2026-03-12 17:13:27 -0600

    changed a path

--- scripts/launch.sh
+++ scripts/launch.sh
@@ -3,7 +3,7 @@ if [ "$1" == "--reset-cache" ]; then
   rm -rf ~/.cache/ov/texturecache
 fi
 python /home/code/h12_sim_scripts/dds_bridge.py & disown
-python3  ~/unitree_sim_isaaclab/sim_main.py \
+python3  /home/code/CL_isaaclab_sim/sim_main.py \
   --device cuda \
   --task Isaac-PickPlace-Cylinder-H12-27dof-Inspire-Joint \
   --enable_inspire_dds \
